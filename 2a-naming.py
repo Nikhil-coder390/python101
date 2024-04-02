c17e12f2f1ece56e87e89322a2e97ba1a3c470c1
Nikhil Sundaraneedi <nikhilnaidu0004@gmail.com>
2024-04-02 17:13:44 +0000

    Update

---
+++ 2a-naming.py
@@ -0,0 +1,12 @@
+sample = 'nikhil'
+sampl = sample.upper()
+print(sampl.lower())
+string = "Hello World "
+print(string.startswith("H")) # Boolean True
+print(string.endswith("d")) # Boolean True
+print(string.find("o")) # int 4
+print(string.index("o")) # int 4
+print(string.startswith("e")) # Boolean False
+print(string.endswith("H")) # Boolean False
+print(string)
+print(string.endswith(" ")) # Boolean True
