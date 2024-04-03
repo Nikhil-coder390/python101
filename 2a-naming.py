71d5654ac8eb67ffa0d652ee4dd1b1b7d42c5f7e
Nikhil Sundaraneedi <nikhilnaidu0004@gmail.com>
2024-04-03 16:20:56 +0000

    Updared

--- 2a-naming.py
+++ 2a-naming.py
@@ -10,3 +10,5 @@ print(string.startswith("e")) # Boolean False
 print(string.endswith("H")) # Boolean False
 print(string)
 print(string.endswith(" ")) # Boolean True
+list_a = [1,2,3]
+print(list_a.index(3))
